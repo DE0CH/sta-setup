1bc70364b76092eb9b3ade6981c627e42c211a76
Deyao Chen <chendeyao000@gmail.com>
2022-04-21 00:35:34 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,28 @@
+cd ${HOME}
+git clone https://github.com/Homebrew/brew .linuxbrew
+.linuxbrew/bin/brew update --force --quiet
+echo 'eval "$(.linuxbrew/bin/brew shellenv)"' >> .profile
+cat <<EOT >> .bashrc
+/bin/zsh
+exit
+EOT
+eval "$(.linuxbrew/bin/brew shellenv)"
+brew install --force-bottle binutils
+brew install --force-bottle gcc
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cd ${HOMEBREW_PREFIX}/bin
+ln -s gcc gcc-11
+ln -s g++ g++-1
+ln -s cpp cpp-11
+ln -s c++ c++-11
+
+DIR=${PWD}
+cd ${HOME}
+REL=$(realpath --relative-to="${HOME}" "${DIR}")
+ln -s ${DIR}/.zshrc .zshrc
+ln -s ${DIR}/.p10k.zsh .p10k.zsh
