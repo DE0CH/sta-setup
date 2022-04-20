e9e2d2bca44e2719f7773371d407f785127eca04
Deyao Chen <chendeyao000@gmail.com>
2022-04-21 01:20:13 +0800

    update dir

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+echo $DIR
+
 cd ${HOME}
 git clone https://github.com/Homebrew/brew .linuxbrew
 .linuxbrew/bin/brew update --force --quiet
